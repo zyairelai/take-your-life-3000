791103f390ecd93f774949af00e3ac356fde0a5d
zyairelai <zyairelai@gmail.com>
2024-06-04 00:43:58 +0800

    Create webscan-class-b.sh

---
+++ webscan/webscan-class-b.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Extract the first and third octets from the input IP address
+ip_prefix=$(echo $1 | cut -d '.' -f 1,3)
+
+# Iterate over the second octet from 1 to 255 and run the webscan command
+for i in {1..255}; do
+    webscan_bulk "$ip_prefix.$i.1"
+done
