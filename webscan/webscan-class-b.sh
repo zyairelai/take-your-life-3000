d05e066e7e2f9933c0caa81fb3237aa37f76639a
zyairelai <zyairelai@gmail.com>
2024-06-04 00:47:35 +0800

    Update webscan-class-b.sh

--- webscan/webscan-class-b.sh
+++ webscan/webscan-class-b.sh
@@ -5,5 +5,5 @@ ip_prefix=$(echo $1 | cut -d '.' -f 1,3)
 
 # Iterate over the second octet from 1 to 255 and run the webscan command
 for i in {1..255}; do
-    webscan_bulk "$ip_prefix.$i.1"
+    webscan-class-c "$ip_prefix.$i.1"
 done
