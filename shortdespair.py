5519212c93fe2cae06a4acf4663e138edb850916
zyairelai <zyairelai@gmail.com>
2025-11-04 21:07:20 +0800

    Update shortdespair.py

--- shortdespair.py
+++ shortdespair.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import time, os
+from datetime import datetime
 try: import ccxt, pandas, requests, socket
 except ImportError:
     print("library not found, run:\npip3 install ccxt pandas requests socket --break-system-packages")
@@ -15,10 +16,11 @@ def telegram_bot_sendtext(bot_message):
     return response.json()
 
 # telegram_bot_sendtext("Telegram works!")
+print("The DESPAIR script is running...\n")
 
 def get_klines(pair, interval):
     tohlcv_colume = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
-    return pandas.DataFrame(ccxt.binance().fetch_ohlcv(pair, interval , limit=200), columns=tohlcv_colume)
+    return pandas.DataFrame(ccxt.binance().fetch_ohlcv(pair, interval , limit=50), columns=tohlcv_colume)
 
 def heikin_ashi(klines):
     heikin_ashi_df = pandas.DataFrame(index=klines.index.values, columns=['ha_open', 'ha_high', 'ha_low', 'ha_close'])
@@ -34,12 +36,11 @@ def heikin_ashi(klines):
     heikin_ashi_df["color"] = heikin_ashi_df.apply(color, axis=1)
     heikin_ashi_df['10EMA'] = klines['close'].ewm(span=10, adjust=False).mean()
     heikin_ashi_df['20EMA'] = klines['close'].ewm(span=20, adjust=False).mean()
-    heikin_ashi_df['100EMA'] = klines['close'].ewm(span=100, adjust=False).mean()
     heikin_ashi_df['25MA'] = klines['close'].rolling(window=25).mean()
     heikin_ashi_df['downtrend'] = heikin_ashi_df.apply(downtrend, axis=1)
     heikin_ashi_df['smooth'] = heikin_ashi_df.apply(smooth_criminal, axis=1)
 
-    result_cols = ['ha_high', 'color', '10EMA', '20EMA', '100EMA', '25MA', 'downtrend', 'smooth']
+    result_cols = ['ha_open', 'ha_high', 'color', '10EMA', '20EMA', '25MA', 'downtrend', 'smooth']
     for col in result_cols: heikin_ashi_df[col] = heikin_ashi_df[col].apply(no_decimal)
     return heikin_ashi_df[result_cols]
 
@@ -56,7 +57,7 @@ def downtrend(HA):
     return HA['25MA'] > HA['20EMA'] and HA['25MA'] > HA['10EMA'] and HA['20EMA'] > HA['10EMA']
 
 def smooth_criminal(HA):
-    return HA['25MA'] > HA['ha_high']
+    return HA['25MA'] > HA['ha_open']
 
 def short_despair(pair):
     direction = heikin_ashi(get_klines(pair, "1h"))
@@ -66,26 +67,23 @@ def short_despair(pair):
 
     condition_1h = direction["color"].iloc[-1] == "RED"
     condition_5m = minute_5m["20EMA"].iloc[-1] > minute_5m["10EMA"].iloc[-1]
-    condition_3m = all(minute_3m["downtrend"].iloc[-3:]) and all(minute_3m["smooth"].iloc[-3:])
-    condition_1m = all(minute_1m["downtrend"].iloc[-5:]) and all(minute_1m["smooth"].iloc[-3:])
+    condition_3m = all(minute_3m["downtrend"].iloc[-3:]) and minute_3m["10EMA"].iloc[-1] > minute_3m["ha_open"].iloc[-1] 
+    condition_1m = all(minute_1m["downtrend"].iloc[-5:]) and all(minute_1m["smooth"].iloc[-5:])
+
+    print("condition_1h: " + str(condition_1h))
+    print("condition_5m: " + str(condition_5m))
+    print("condition_3m: " + str(condition_3m))
+    print("condition_1m: " + str(condition_1m))
+    print("Last action executed @ " + datetime.now().strftime("%H:%M:%S") + "\n")
 
     if condition_1h and condition_5m and condition_3m and condition_1m:
         telegram_bot_sendtext("💥 TIME TO SHORT 💥")
         exit()
 
-def cooldown_period(pair):
-    minute_1m = heikin_ashi(get_klines(pair, "1m"))
-    if minute_1m["ha_high"].iloc[-1] > minute_1m["100EMA"].iloc[-1]:
-        telegram_bot_sendtext("🥶 COOLDOWN RESET 🥶")
-        exit()
-
-# print("The DESPAIR script is running...\n")
-print("Waiting for Cooldown...\n")
-
 try:
     while True:
         try:
-            cooldown_period("BTCUSDC")
+            short_despair("BTCUSDC")
             time.sleep(10)
         except (ccxt.RequestTimeout, ccxt.NetworkError, ConnectionResetError, socket.timeout,
                 requests.exceptions.RequestException) as e:
