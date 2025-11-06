300c399e269a242f9e2690609fffcb5de7ed2340
zyairelai <zyairelai@gmail.com>
2025-11-06 10:40:54 +0800

    Update cooldown.py

--- quant/cooldown.py
+++ quant/cooldown.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-import time, os
-try: import ccxt, pandas, requests, socket
-except ImportError:
-    print("library not found, run:\npip3 install ccxt pandas requests socket --break-system-packages")
-    exit(1)
+import ccxt, pandas, requests, socket, time, os
+
 
 def telegram_bot_sendtext(bot_message):
     print(bot_message)
@@ -14,53 +11,72 @@ def telegram_bot_sendtext(bot_message):
     response = requests.get(send_text)
     return response.json()
 
+
 # telegram_bot_sendtext("Telegram works!")
-print("The COOLDOWN script is running...\n")
+
 
 def get_klines(pair, interval):
     tohlcv_colume = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
-    return pandas.DataFrame(ccxt.binance().fetch_ohlcv(pair, interval , limit=200), columns=tohlcv_colume)
+    return pandas.DataFrame(ccxt.binance().fetch_ohlcv(pair,
+                                                       interval,
+                                                       limit=200),
+                            columns=tohlcv_colume)
+
 
 def heikin_ashi(klines):
-    heikin_ashi_df = pandas.DataFrame(index=klines.index.values, columns=['ha_open', 'ha_high', 'ha_low', 'ha_close'])
-    heikin_ashi_df['ha_close'] = (klines['open'] + klines['high'] + klines['low'] + klines['close']) / 4
+    heikin_ashi_df = pandas.DataFrame(
+        index=klines.index.values,
+        columns=['ha_open', 'ha_high', 'ha_low', 'ha_close'])
+    heikin_ashi_df['ha_close'] = (klines['open'] + klines['high'] +
+                                  klines['low'] + klines['close']) / 4
 
     for i in range(len(klines)):
         if i == 0: heikin_ashi_df.iat[0, 0] = klines['open'].iloc[0]
-        else: heikin_ashi_df.iat[i, 0] = (heikin_ashi_df.iat[i-1, 0] + heikin_ashi_df.iat[i-1, 3]) / 2
+        else:
+            heikin_ashi_df.iat[i, 0] = (heikin_ashi_df.iat[i - 1, 0] +
+                                        heikin_ashi_df.iat[i - 1, 3]) / 2
 
-    heikin_ashi_df.insert(0,'timestamp', klines['timestamp'])
-    heikin_ashi_df['ha_high'] = heikin_ashi_df.loc[:, ['ha_open', 'ha_close']].join(klines['high']).max(axis=1)
-    heikin_ashi_df['ha_low']  = heikin_ashi_df.loc[:, ['ha_open', 'ha_close']].join(klines['low']).min(axis=1)
-    heikin_ashi_df['10EMA'] = klines['close'].ewm(span=10, adjust=False).mean()
-    heikin_ashi_df['20EMA'] = klines['close'].ewm(span=20, adjust=False).mean()
-    heikin_ashi_df['25MA'] = klines['close'].rolling(window=25).mean()
-    heikin_ashi_df['100EMA'] = klines['close'].ewm(span=100, adjust=False).mean()
+    heikin_ashi_df.insert(0, 'timestamp', klines['timestamp'])
+    heikin_ashi_df[
+        'ha_high'] = heikin_ashi_df.loc[:, ['ha_open', 'ha_close']].join(
+            klines['high']).max(axis=1)
+    heikin_ashi_df[
+        'ha_low'] = heikin_ashi_df.loc[:, ['ha_open', 'ha_close']].join(
+            klines['low']).min(axis=1)
+    heikin_ashi_df['100EMA'] = klines['close'].ewm(span=100,
+                                                   adjust=False).mean()
 
-    result_cols = ['ha_high', 'ha_low', 'ha_open', 'ha_close', '25MA', '100EMA']
-    for col in result_cols: heikin_ashi_df[col] = heikin_ashi_df[col].apply(no_decimal)
+    result_cols = ['ha_high', '100EMA']
+    for col in result_cols:
+        heikin_ashi_df[col] = heikin_ashi_df[col].apply(no_decimal)
     return heikin_ashi_df[result_cols]
+
 
 def no_decimal(val):
     if isinstance(val, float) and not pandas.isna(val): return round(val)
     return val
 
+
 def cooldown_period(pair):
     cooldown = heikin_ashi(get_klines(pair, "1m"))
-    # print(cooldown)
     if cooldown["ha_high"].iloc[-1] > cooldown["100EMA"].iloc[-1]:
         telegram_bot_sendtext("🥶 COOLDOWN RESET 🥶")
-        exit()
+        return True
+    return False
 
-try:
-    while True:
-        try:
-            cooldown_period("BTCUSDC")
-            time.sleep(10)
-        except (ccxt.RequestTimeout, ccxt.NetworkError, ConnectionResetError, socket.timeout,
-                requests.exceptions.RequestException) as e:
-            print(f"Network error: {e}")
-            telegram_bot_sendtext(f"Network error: {e}")
-            time.sleep(30)
-            continue
-except KeyboardInterrupt: print("\n\nAborted.\n")
+
+if __name__ == '__main__':
+    try:
+        while True:
+            try:
+                cooldown_period("BTCUSDC")
+                time.sleep(10)
+            except (ccxt.RequestTimeout, ccxt.NetworkError,
+                    ConnectionResetError, socket.timeout,
+                    requests.exceptions.RequestException) as e:
+                print(f"Network error: {e}")
+                telegram_bot_sendtext(f"Network error: {e}")
+                time.sleep(60)
+                continue
+    except KeyboardInterrupt:
+        print("\n\nAborted.\n")
